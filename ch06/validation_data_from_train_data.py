597e0a933030435b3998f3a4f9bf7929c5fb9297
yammmt <cu.cs.y20@gmail.com>
2019-06-18 21:29:37 +0900

    Add chapter 06

---
+++ ch06/validation_data_from_train_data.py
@@ -0,0 +1,15 @@
+# pp. 198 - 199
+
+# TODO: import common library (load_mnist, shuffle_dataset)
+
+(x_train, t_train), (x_test, t_test) = load_mnist()
+
+x_train, t_train = shuffle_dataset(x_train, t_train)
+
+validation_rate = 0.20
+validation_num = int(x_train.shape[0] * validation_rate)
+
+x_val = x_train[:validation_num]
+t_val = t_train[:validation_num]
+x_train = x_train[validation_num:]
+t_train = t_train[validation_num:]
