3a38290ac1299d2280125511e47e8460707acc40
yammmt <cu.cs.y20@gmail.com>
2019-06-30 13:44:02 +0900

    Add chapter 07

---
+++ ch07/use_im2col.py
@@ -0,0 +1,15 @@
+# pp. 224-
+
+import numpy as np
+import os
+import sys
+sys.path.append(os.pardir)
+from common.util import im2col
+
+x1 = np.random.rand(1, 3, 7, 7)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape)
+
+x2 = np.random.rand(10, 3, 7, 7)
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape)
